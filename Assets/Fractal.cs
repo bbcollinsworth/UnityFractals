1858afee18a4deab2fbc57ccc367a95319c97990
monotoan <bcollinsworth@gmail.com>
2017-04-23 11:00:34 -0700

    Added randomness controlls for branch length and angle

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -34,15 +34,13 @@ public class Fractal : MonoBehaviour
 		}
 
 		//Matrix4x4[] allMatrices = branches.ToArray () as Matrix4x4[];
-		Graphics.DrawMeshInstanced (meshToDraw, 0, mat, allMatrices);
+		//Graphics.DrawMeshInstanced (meshToDraw, 0, mat, allMatrices);
 	}
 
 	void OnDrawGizmos ()
 	{
 		branches = new ArrayList ();
-
 		Branch (transform.position, 2, Quaternion.LookRotation (Vector3.forward, Vector3.up));
-
 	}
 
 	void Branch (Vector3 start, float length, Quaternion rot)
@@ -52,27 +50,29 @@ public class Fractal : MonoBehaviour
 		length = length + length * lRand;
 		var direction = rot * (Vector3.up * length);
 		var end = start + direction;
-		//DrawVector (start, direction, Color.white);
+		DrawVector (start, direction, Color.white);
 		var rad = length * 0.1f;
 		Vector3 scale = new Vector3 (rad, length * 0.5f, rad);
 		Matrix4x4 tMatrix = Matrix4x4.TRS (start + (direction * 0.5f), rot, scale);
 
 		branches.Add (tMatrix);
-		//Graphics.DrawMesh (meshToDraw, tMatrix, mat, 0);
-		//Matrix4x4[]
-		//m = new Matrix4x4[]{ tMatrix };
-		//Graphics.DrawMeshInstanced (meshToDraw, 0, mat, m);
 
 		if (length > minSize) {
 			if (start == Vector3.zero)
 				start = transform.right;
 			
 			Vector3 directionNormal = Vector3.Cross (direction, start).normalized;
-			directionNormal = Quaternion.AngleAxis (angle, direction) * directionNormal;
+			directionNormal = Quaternion.AngleAxis (GetRandomAngle (true), direction) * directionNormal;
 			//DrawVector (end, directionNormal, Color.red);
-			Branch (end, length * 0.8f, Quaternion.AngleAxis (angle * 0.5f, directionNormal) * rot);
-			Branch (end, length * 0.8f, Quaternion.AngleAxis (angle * -0.5f, directionNormal) * rot);
+			Branch (end, length * 0.8f, Quaternion.AngleAxis (GetRandomAngle () * 0.5f, directionNormal) * rot);
+			Branch (end, length * 0.8f, Quaternion.AngleAxis (GetRandomAngle () * -0.5f, directionNormal) * rot);
 		}
+	}
+
+	float GetRandomAngle (bool randomSign = false)
+	{
+		var randSign = randomSign ? Random.value * 2 - 1 : 1;
+		return angle * (1 - Random.value * angleRandomness * Mathf.Sign (randSign));
 	}
 
 	void DrawVector (Vector3 start, Vector3 end, Color color)
